287e0ad38225ec40d68c70606bf7d1b81e91eb7f
nirvik <nirvik1993@gmail.com>
2015-04-14 21:56:04 +0530

    added Pipe

--- snek/net.py
+++ snek/net.py
@@ -1,12 +1,46 @@
-import socket as sck
+import socket
+import pickle
+import threading 
+import time 
 
-
-class Pipe():
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
-        self._sock = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-
-    def connect(self, addr):
-        while
+class Pipes(socket.socket):
         
+    def __init__(self,event,host,port):
+        super(Pipes,self).__init__(socket.AF_INET,socket.SOCK_DGRAM)
+        self.bind((host,port))
+        self.settimeout(100)
+        self.event = event 
+        self.host = host
+        self.port = port 
+        print 'Created socket'
+
+    def negotiate(self):
+        self.connect((self.host,self.port))
+        return True
+    
+    def sendMessage(self,dest_host, dest_port):
+        print 'Establishing Connection'
+        self.sendto(self.event,(dest_host,dest_port))
+
+    def submitReactor(self,recv_buffer=4096,delim='\n'):
+        buffer = ''
+        while True:
+            conn,addr = self.recvfrom(recv_buffer)
+            if conn:
+                buffer += conn
+                print buffer 
+            else:
+                break
+        print ' Received Connection and says : ' + buffer
+
+    def receive(self):
+        print 'Waiting.. To receive'
+        t = threading.Thread(target = self.submitReactor , args = ())
+        t.daemon = False 
+        t.start()
+
+    def __del__(self):
+        self.close()
+        print 'sock closed'
+
+
